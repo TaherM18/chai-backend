8832ef37d2a70efacfa767676ee22ca218cd76ec
Taher Maimoon <tmaimoon18@gmail.com>
2025-07-28 19:17:25 +0530

    Add multer middleware. Add cloudinary config and upload function

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "./public/temp");
+    },
+    filename: function(req, file, callback) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        callback(null, file.filename);
+    }
+});
+
+export const upload = multer({ storage: storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+async function uploadToCloudinary(localFilePath) {
+    if (!localFilePath) {
+        return null;
+    }
+    try {
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        });
+        console.log("file uploaded to Cloudinary", response.url);
+        return response;
+    }
+    catch (error) {
+        console.log();
+        // remove the local file as upload operation failed
+        fs.unlink(localFilePath);
+        return null;
+    }
+}
+
+export { uploadToCloudinary }
