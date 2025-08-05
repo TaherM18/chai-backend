195751da104e362486a8b67926fefd878731b584
Taher Maimoon <tmaimoon18@gmail.com>
2025-08-05 18:10:56 +0530

    Refactor user routes: add changeAvatar, getCurrentUser, updateUserAccount, getUserChannelProfile, and getWatchHistory endpoints.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,12 @@ import {
     registerUser, 
     logoutUser, 
     refreshAccessToken, 
-    changePassword
+    changePassword,
+    changeAvatar,
+    updateUserAccount,
+    getUserChannelProfile,
+    getCurrentUser,
+    getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,6 +36,17 @@ userRouter.route("/logout").post(verifyJWT, logoutUser);
 
 userRouter.route("/refresh-token").post(refreshAccessToken);
 
-userRouter.route("/change-password").post(verifyJWT, changePassword);;
+userRouter.route("/change-password").post(verifyJWT, changePassword);
+
+userRouter.route("/save-avatar").patch(verifyJWT, upload.single("avatar"), changeAvatar);
+
+userRouter.route("/account").get(verifyJWT, getCurrentUser);
+
+userRouter.route("/update-account").patch(verifyJWT, updateUserAccount);
+
+userRouter.route("/channel-profile/:username").get(verifyJWT, getUserChannelProfile);
+
+userRouter.route("/watch-history").get(verifyJWT, getWatchHistory);
+
 
 export default userRouter;
